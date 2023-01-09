87caa2f1132e0b89b301fb96b8a37f97a6d6cfe5
devanshu121 <devanshumishra543@gmail.com>
2023-01-09 21:30:26 +0530

    course5i-cookiecutter and yaml

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project Is to Demostrate Mlops Using MLFLOW',
+    author='course5i',
+    license='MIT',
+)
